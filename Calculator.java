db5c8c2a95a3062d21b167e845d98816e01dcc7f
Lerokaa <valerygetman@gmail.com>
2025-05-18 15:11:08 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,7 @@
 public class Calculator {
 
     private static int currentBase = 10;
-    public static void Main(String[] args)
+    public static void main(String[] args)
     {
         System.out.println("Инженерный калькулятор");
         System.out.println("Поддерживаемые системы счисления: 2, 8, 10, 16");
