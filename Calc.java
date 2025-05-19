42bba629c1440e2b10cf51e7e53b13e193542829
Lerokaa <151133526+Lerokaa@users.noreply.github.com>
2025-05-19 10:25:14 +0300

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -46,27 +46,14 @@ public class Calc {
         return 0;
     }
 
-    public void getResult() {
-        try {
-            InputHandler handler = new InputHandler(scanner);
-            System.out.print("Введите систему счисления (2/8/10/16): ");
-            int radix = Integer.parseInt(scanner.nextLine());
-    
-            int a = handler.getNumber(radix, "Введите первое число: ");
-            String sign = handler.getSign("Введите знак операции (+ - * /): ");
-            int b = handler.getNumber(radix, "Введите второе число: ");
-    
-            int result = solve(a, b, sign);
-    
-            System.out.println("Результат:");
-            System.out.println("2: " + Integer.toBinaryString(result));
-            System.out.println("8: " + Integer.toOctalString(result));
-            System.out.println("10: " + result);
-            System.out.println("16: " + Integer.toHexString(result));
-        } 
-            catch (Exception e) {
-            System.out.println("Ошибка при выполнении операции: " + e.getMessage());
-        }
-    }
-}
+    public void getResult () throws Exception{
+        result = this.Solve();
+        String resultB = Integer.toBinaryString(result);
+        String resultO = Integer.toOctalString(result);
+        String resultH = Integer.toHexString(result);
 
+        System.out.println("Результат выполнения:");
+        System.out.println("Bin: " + resultB + "  Oct: "+resultO + "  Dec: " + result + "  Hex: " + resultH);
+    }
+
+}
